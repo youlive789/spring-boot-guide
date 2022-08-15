5aac5a1376be23e4c656712de97731aa9a1eb63b
gwkim <youlive789@gmail.com>
2022-08-15 13:38:08 +0900

    feat: Chapter 7 controller test

--- src/main/java/com/spring/guide/guide/six/data/dto/ProductResponse.java
+++ src/main/java/com/spring/guide/guide/six/data/dto/ProductResponse.java
@@ -1,5 +1,6 @@
 package com.spring.guide.guide.six.data.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,6 +9,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@AllArgsConstructor
 @NoArgsConstructor
 public class ProductResponse {
     private Long id;
---
+++ src/test/java/com/spring/guide/guide/six/ProductControllerTests.java
@@ -0,0 +1,51 @@
+package com.spring.guide.guide.six;
+
+import static org.mockito.ArgumentMatchers.any;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.spring.guide.guide.six.data.dto.ProductResponse;
+import com.spring.guide.guide.six.service.impl.ProductServiceImpl;
+
+@WebMvcTest(ProductController.class)
+public class ProductControllerTests {
+    
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    ProductServiceImpl productService;
+
+    @Test
+    @DisplayName("상품 조회 API 테스트")
+    void getProductTest() throws Exception {
+
+        Long productId = 123L;
+        String productName = "test";
+
+        // given
+        given(productService.getProduct(productId)).willReturn(new ProductResponse(productId, productName));
+
+        // when
+        mvc.perform(get("/product/v1/" + productId))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.id").exists())
+            .andExpect(jsonPath("$.name").exists())
+            .andDo(print());
+
+        // then
+        verify(productService).getProduct(productId);
+    }
+
+}
